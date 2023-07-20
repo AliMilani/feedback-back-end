2364e0c1c6d618488d3e03357248ad340f49c095
Ali Milani <amilani.ir@gmail.com>
2023-07-20 14:51:34 +0330

    refactor: api error middleware

--- src/http/middlewares/apiErrors.middleware.js
+++ src/http/middlewares/apiErrors.middleware.js
@@ -2,29 +2,35 @@ const express = require("express");
 require("express-async-errors");
 const response = require("../../utils/response.utils");
 const { Error: MongooseError } = require("mongoose");
+const logger = require("../../lib/logger");
 
 const apiError = (err, req, res, next) => {
-  if (err.type === "entity.parse.failed") {
+  console.log("we are in apiError middleware");
+  if (err.type === "entity.parse.failed")
     return response(res, {
       code: "JSON_SYNTAX_ERROR",
     });
-  }
-  if ((err.type = "entity.too.large")) {
+
+  if (err.type == "entity.too.large")
     return response(res, {
       code: "PAYLOAD_TOO_LARGE",
     });
-  }
+
+  const { stack, message } = err;
+
   if (err instanceof MongooseError)
     response(res, {
       code: "DATABASE_ERROR",
-      errors: process.env.NODE_ENV === "development" ? err : undefined,
+      errors:
+        process.env.NODE_ENV === "development" ? { message, stack } : undefined,
     });
   else
     response(res, {
       code: "SERVER_ERROR",
-      errors: process.env.NODE_ENV === "development" ? err : undefined,
+      errors:
+        process.env.NODE_ENV === "development" ? { message, stack } : undefined,
     });
-  console.error(`${err.message} \n\n ${err.stack || ""}`);
+  logger.error(`${message} \n\n ${stack || ""}`);
   process.exit(1);
 };
 
