c334689176e39adbae5141b8448b755012e72e46
Ali Milani <amilani.ir@gmail.com>
2023-07-23 12:46:52 +0330

    refactor: remove catch block from server module

--- src/server.js
+++ src/server.js
@@ -4,14 +4,9 @@ const validateEnv = require("./startup/validateEnv");
 // const validateConfig = require("./startup/validateConfig");
 const Application = require("./app");
 const logger = require("./lib/logger");
-
 validateEnv(process.env);
 new Application(config.get("mongodbURI"), config.get("port"))
   .start()
   .then(() => {
       logger.info("Application started successfully.");
   })
-  .catch((err) => {
-    logger.error(err.message);
-    process.exit(1);
-  });
